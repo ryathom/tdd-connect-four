587fbba445d03bbfd0d8498ef0a90a16ab9eca53
Ryan Thomas <ryanthomasj15@gmail.com>
2022-10-01 14:04:19 +0100

    Tests for first board update

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -21,4 +21,20 @@ describe ConnectFour do
       expect(game.board.flatten.all?(nil)).to be true
     end
   end
+
+  context 'when board is updated once' do
+    before do
+      pos = 3
+      symbol = 'A'
+      game.update_board(pos, symbol)
+    end
+
+    it 'symbol in correct space' do
+      expect(game.board[0][pos]).to eql(symbol)
+    end
+
+    it 'no other spaces changed' do
+      expect(game.board.flatten.compact.length).to eq(1)
+    end
+  end
 end
